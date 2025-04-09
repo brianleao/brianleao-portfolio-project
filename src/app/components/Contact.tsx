ce5efa3a4a53b4c317570f7cc929d615688dd229
Brian Leao <brianleao@icloud.com>
2025-04-09 16:45:15 -0300

    hover add in contacts div

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -18,7 +18,7 @@
                 {
                     socialMedias.map((social) => (
                         <div key={social.id}>
-                            <div className="bg-[#131313] flex items-center gap-[1rem] rounded-[1rem] p-[2.3rem] sm:p-[7rem] sm:rounded-[1.25rem] border-[0.3rem] sm:border-[0.625rem] border-[#161616] font-normal text-[clamp(1rem,2vw,4rem)]">
+                            <div className="bg-[#131313] flex flex-wrap items-center gap-[1rem] rounded-[1rem] p-[2.3rem] sm:p-[7rem] sm:rounded-[1.25rem] border-[0.3rem] sm:border-[0.625rem] border-[#161616] font-normal text-[clamp(1.5rem,2vw,4rem)] hover:bg-[#1b1b1b] duration-600 cursor-pointer">
                                 {social.icon}
                                 <a href={social.link} target="_blank" rel="noopener noreferrer">{social.social}</a>
                             </div>
