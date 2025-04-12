317ab6a3474cc94de81ab3af568ec1d3d863f2b7
Brian Leao <brianleao@icloud.com>
2025-04-12 00:58:18 -0300

    ajusts in responsive icon

--- src/app/components/ContactMe.tsx
+++ src/app/components/ContactMe.tsx
@@ -1,25 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { CaretDown } from "@phosphor-icons/react";
 import { motion } from "framer-motion";
 
 export default function ContactMe() {
+  const [iconSize, setIconSize] = useState(32); // valor inicial seguro para o SSR
+
+  useEffect(() => {
+    const vw = window.innerWidth;
+    const size = Math.min(Math.max(vw * 0.01, 32), 48);
+    setIconSize(size);
+  }, []);
+
   return (
     <div className="flex justify-center">
-    <a href="#contact" className="flex flex-col justify-center items-center cursor-pointer w-max">
-      <span className="text-[clamp(1.5rem,3vw,1.5625rem)] font-extralight mt-[2.5rem] sm:mt-[6.5rem]">
-        Me contate
-      </span>
+      <a href="#contact" className="flex flex-col justify-center items-center cursor-pointer w-max">
+        <span className="text-[clamp(1.5rem,2vw,4rem)] font-extralight mt-[5rem]">Me contate</span>
 
-      <motion.div
-        animate={{ y: [0, -10, 0] }}
-        transition={{
-          duration: 1.5,
-          repeat: Infinity,
-          ease: "easeInOut",
-        }}
-      >
-        <CaretDown size={32} color="#e3e3e3" weight="light" className="mb-[5rem]" />
-      </motion.div>
-    </a>
+        <motion.div
+          animate={{ y: [0, -10, 0] }}
+          transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut" }}
+        >
+          <CaretDown
+            size={iconSize}
+            color="#e3e3e3"
+            weight="light"
+            className="mb-[clamp(2.5rem,10vh,5rem)] select-none pointer-events-none"
+          />
+        </motion.div>
+      </a>
     </div>
   );
 }
