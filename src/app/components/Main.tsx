520d51c8d88125d06f51270cb935d609fc49356e
Brian Leao <brianleao@icloud.com>
2025-04-12 00:14:18 -0300

    ajusts in tailwind.config.ts

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -2,7 +2,7 @@
 
 export default function Aside() {
   return (
-    <main className=" px-[1.55rem] sm:mt-[4rem] text-[clamp(2rem,12vw,5.25rem)] font-bold flex flex-col w-max">
+    <main className=" px-[1.55rem] sm:mt-[4rem] text-[clamp(3rem,8vw,10.25rem)] font-bold flex flex-col w-max">
       <p>Olá, eu sou o</p>
       <p className="text-greenPantano text-shadow-extern2">Brian Leão</p>
     </main>
---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+      'large27': '2560px',
+    },
+    extend: {
+      keyframes: {},
+      animation: {},
+    },
+  },
+  plugins: [],
+};
+
+export default config;
