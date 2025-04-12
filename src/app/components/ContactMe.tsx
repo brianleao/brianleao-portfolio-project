8f8a28d159c2266d64a5c228914ae2b459e87318
Brian Leao <brianleao@icloud.com>
2025-04-11 23:40:15 -0300

    ajust in contactMe component

--- src/app/components/ContactMe.tsx
+++ src/app/components/ContactMe.tsx
@@ -3,7 +3,8 @@ import { motion } from "framer-motion";
 
 export default function ContactMe() {
   return (
-    <a href="#contact" className="flex flex-col justify-center items-center cursor-pointer">
+    <div className="flex justify-center">
+    <a href="#contact" className="flex flex-col justify-center items-center cursor-pointer w-max">
       <span className="text-[clamp(1.5rem,3vw,1.5625rem)] font-extralight mt-[2.5rem] sm:mt-[6.5rem]">
         Me contate
       </span>
@@ -19,5 +20,6 @@ export default function ContactMe() {
         <CaretDown size={32} color="#e3e3e3" weight="light" className="mb-[5rem]" />
       </motion.div>
     </a>
+    </div>
   );
 }
