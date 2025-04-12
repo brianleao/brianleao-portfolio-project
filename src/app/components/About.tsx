16c7e5bf7021004cbd92cac706b8c873de896afb
Brian Leao <brianleao@icloud.com>
2025-04-12 01:07:22 -0300

    ajusts in about.tsx

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -65,7 +65,7 @@ export default function About() {
         <div ref={containerRef} className="flex gap-[3.5rem] whitespace-nowrap w-max">
           
           {duplicated.map((tech, index) => (
-            <div key={index} className="bg-[#191919] font-bold py-4 px-5 rounded-[0.625rem] text-[clamp(1rem,1vw,1rem)]">
+            <div key={index} className="bg-[#191919] font-bold py-4 px-5 rounded-[0.625rem] text-[clamp(1rem,1.2vw,6rem)]">
               <span>{tech.name}</span>
             </div>
           ))}
