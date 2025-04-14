1581ac6e16a0ea86dcc9d19c827dd8f71436c9c0
Brian Leao <brianleao@icloud.com>
2025-04-13 22:10:39 -0300

    ajusts in margin top

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -9,8 +9,8 @@ export default function Experience() {
 
     return(
         <section id="experiences" className="scroll-mt-5 max-w-5xl mx-auto">
-            <h2 className="text-center font-bold text-[clamp(2.5rem,3vw,3rem)] mt-[10rem]">Experiências</h2>
-            <div className="flex gap-4 flex-wrap justify-center mt-[2.69rem] mx-[2rem]">
+            <h2 className="text-center font-bold text-[clamp(2.5rem,3vw,3rem)] mt-[5rem]">Experiências</h2>
+            <div className="flex gap-4 flex-wrap justify-center mt-[2rem] mx-[2rem]">
                 {
                     experiences.map((experience) => (
                         <div key={experience.id}>
